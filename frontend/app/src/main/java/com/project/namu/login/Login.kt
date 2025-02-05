12fd7007228733772c6f12f63abe8eac2f8fdcd8
lee_jun_young <ljy03@pusan.ac.kr>
2025-02-05 15:55:56 +0900

    login scroll

--- frontend/app/src/main/java/com/project/namu/login/Login.kt
+++ frontend/app/src/main/java/com/project/namu/login/Login.kt
@@ -14,7 +14,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Lock
@@ -69,7 +71,11 @@ fun Login(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.White)) {
+            .background(color = Color.White)
+            .verticalScroll(rememberScrollState()), // 스크롤 적용
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
         Spacer(modifier = Modifier.height(100.dp))
 
         Image(
@@ -228,6 +234,8 @@ fun Login(
         Spacer(modifier = Modifier.height(10.dp))
 
         LogSignButton(type = "goSign", color = "white", popUpViewModel = PopUpViewModel, LogInViewModel = LogInViewModel, signInViewModel = signInViewModel, navController = navController )
+
+        Spacer(modifier = Modifier.height(30.dp)) // 마지막 여백 추가
     }
 
 }
